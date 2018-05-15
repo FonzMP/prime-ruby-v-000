1bcf0804ecd270fdfe9d95646020059d7e2eaa15
Alex Wilson <fonzmp@gmail.com>
2018-05-15 19:25:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,16 +2,20 @@
 def prime?(int)
   i = 2
   storage = []
-  while i <= int
-    if int % i == 0
-      storage.push(i)
-    end
-    i += 1
-  end
-  array_length = storage.length
-  if array_length >= 2
+  if i < 0
     return false
   else
-    return true
+    while i <= int
+      if int % i == 0
+        storage.push(i)
+      end
+      i += 1
+    end
+    array_length = storage.length
+    if array_length >= 2
+      return false
+    else
+      return true
+    end
   end
 end
